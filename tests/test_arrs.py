89ac23e40b4ec1e5f63e3e0445cb7f2ec6d3e4b1
Anton Vedischev <vedishchevanton@gmail.com>
2023-02-19 23:06:56 +0300

    change test_get()

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,6 +4,7 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], -1, "test") == 'test'
+    assert arrs.get([1, 2, 3], 0, "test") == 1
 
 
 def test_slice():
@@ -13,5 +14,3 @@ def test_slice():
     assert arrs.my_slice([], 0) == []
     assert arrs.my_slice([1, 2, 3], -2) == [2, 3]
     assert arrs.my_slice([1, 2, 3], -1) == [3]
-
-
